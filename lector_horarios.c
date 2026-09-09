71bcb7a2b6b60aefdd7d562361f374ba347dfc58
gabo <g.morales.1@estudiantec.cr>
2026-09-09 12:06:39 -0600

    lector de horarios dividido por ,

--- lector_horarios.c
+++ lector_horarios.c
@@ -2,26 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 
+void procesar_linea(char *linea){
+    char *ptr = linea;
+    char token[512];
+    int idx = 0;
+    int entre_comillas = 0;
+
+    while (*ptr != '\0'){
+        if (*ptr == '"'){
+            entre_comillas = !entre_comillas;
+        }
+        else if (*ptr == ',' && !entre_comillas){
+            token[idx] = '\0';
+            printf("Dato:%s\t", token);
+            idx = 0;
+        }
+        else {
+            if (idx < (int)sizeof(token)-1){
+                token[idx++] = *ptr;
+            }
+        }
+        ptr++;
+    }
+    token [idx] = '\0';
+    printf("Dato: %s\n", token);
+
+}
 
 
 int main (){
     FILE *archivohorario = fopen("horarios.csv", "r");
     if (archivohorario == NULL){
-        printf ("Error al abrir el archivo");
+        fprintf (stderr, "Error al abrir el archivo");
         return 1;
     }
-    char buffer[1024];
+    char buffer[2048];
 
     while (fgets(buffer, sizeof(buffer), archivohorario)){
         buffer [strcspn (buffer, "\r\n")] = 0;
-
-        char *token = strtok(buffer, ",");
-
-        while (token != NULL){
-            printf("Dato: %s\t", token);
-            token = strtok(NULL, ",");
-        }
-        printf("\n");
+        procesar_linea(buffer);
     }
 
     fclose(archivohorario);
