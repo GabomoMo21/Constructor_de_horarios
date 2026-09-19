56a5d04c52173ac9d01fa9fcaf248b309dc6b2b6
Yazar <chrisyazar@gmail.com>
2026-09-19 01:00:33 -0600

    feat(choques): agregar archivo de cabecera choques.h con prototipos de funciones

---
+++ choques.h
@@ -0,0 +1,10 @@
+#ifndef CHOQUES_H
+#define CHOQUES_H
+
+#include "horarios.h"
+
+int horarios_chocan(Horario h1, Horario h2);
+
+void detectar_choques_catalogo(Curso catalogo[], int total_cursos);
+
+#endif
