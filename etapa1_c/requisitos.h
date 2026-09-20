ac8bc697676d27b4c65778976eac48afb3660788
ManlowWhy <olmansr2007@gmail.com>
2026-09-20 04:08:09 -0600

    funciones de validacion

---
+++ etapa1_c/requisitos.h
@@ -0,0 +1,9 @@
+#ifndef REQUISITOS_H
+#define REQUISITOS_H
+
+#include "estructuras.h"
+
+void validar_requisitos_catalogo(Curso catalogo[], int total_cursos,
+                                  char historial[][MAX_CODIGO], int total_historial);
+
+#endif
